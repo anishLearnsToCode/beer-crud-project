515145dcf7b8eeae6e9bc8a6a4222e116899a85f
anishLearnsToCode <anish_@outlook.com>
2021-09-25 15:54:33 +0200

    refactors database.ts file

--- database.ts
+++ database.ts
@@ -1,7 +1,7 @@
-var sqlite3 = require('sqlite3').verbose()
-var md5 = require('md5')
+const sqlite3 = require('sqlite3').verbose();
+const md5 = require('md5');
 
-const DBSOURCE = "db.sqlite"
+const DBSOURCE = "db.sqlite";
 
 const beers = [
     {
@@ -82,4 +82,3 @@ let db = new sqlite3.Database(DBSOURCE, (err) => {
 
 
 module.exports = db
-
